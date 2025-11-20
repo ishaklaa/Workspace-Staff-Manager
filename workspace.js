d346bed9a3a6d7f5f0f37fb0f904225c49729bf3
laaouafi ishak <laaouafi6@gmail.com>
2025-11-20 17:36:12 +0100

    jai ajouter des fonctions de validation

--- workspace.js
+++ workspace.js
@@ -2,19 +2,32 @@ const addModal = document.getElementById("addModal")
 const addButton = document.getElementById("addButton")
 const expButton = document.getElementById("expButton")
 const closeAddModal = document.getElementById("closeModal")
-const expContainer = document.getElementById("experiencesContainer")
-const rolRegex = /^[A-Za-z]+$/;
-const nameRegex = /^[A-Za-z\s]+$/;
-const urlRegex = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\/.*)?$/;
-let namee = document.getElementById("memberNameInput")
-let url = document.getElementById("memberUrlinput")
-let addWorkerForm = document.getElementById("addWorkerForm")
+const experiencesContainer = document.getElementById("experiencesContainer")
+const addWorkerForm = document.getElementById("addWorkerForm")
 const errorNameSpan = document.getElementById("nameError")
 const errorRoleSpan = document.getElementById("roleError")
 const errorUrlSpan = document.getElementById("urlError")
+const errorEmailSpan = document.getElementById("EmailError")
+const errorNumberSpan = document.getElementById("NumberError")
+let errorStartSpan
+// const errorEndSpan = document.getElementById("dateEndErr")
+const namee = document.getElementById("memberNameInput")
+const url = document.getElementById("memberUrlinput")
+const Email = document.getElementById('memberEmilInput')
+const PNumber = document.getElementById('memberNumberInput')
+let expsContainer = document.querySelectorAll(".experiences-container")
+// let ExperienceRole = document.querySelectorAll("#ExperienceRole")
+let currentRole = document.getElementById("currentRole")
+let memberImage = document.getElementById("memberImage")
+let dateStart
+let dateEnd
+
+const nameRegex = /^[A-Za-z\s]+$/;
+const urlRegex = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\/.*)?$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const numberRegex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
+
 let employés = []
-
-
 
 function openModal(modal) {
     modal.style.display = "flex"
@@ -38,51 +51,146 @@ function urlCheck() {
         return true
     }
 }
+function emailCheck() {
+    if (!emailRegex.test(Email.value)) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+function numberCheck() {
+    if (!numberRegex.test(PNumber.value)) {
+        return false
+    }
+    else {
+        return true
+
+    }
+}
 function regexCheck() {
     if (!nameCheck()) {
         errorNameSpan.textContent = "invalid name"
+        return true
     }
-    else {
-        errorNameSpan.textContent = ""
+    if (!urlCheck()) {
+        errorUrlSpan.textContent = "invalid Url"
+    }
+    if (!emailCheck()) {
+        errorEmailSpan.textContent = "invalid email"
+    }
+    if (!numberCheck()) {
+        errorNumberSpan.textContent = "invalid number"
     }
 
-    if (!urlCheck()) {
-        errorUrlSpan.textContent = "invalid url"
-    }
-    else {
-        errorUrlSpan.textContent = ""
-    }
+
+
 
 }
 function saveData(data) {
     localStorage.setItem("data", JSON.stringify(data))
 }
 function experienceDiv() {
-    let template=document.createElement("div")
-    template.className="experiences-container"
-    template.innerHTML= `
+    let template = document.createElement("div")
+    template.className = "experiences-container"
+    template.innerHTML = `
                     <label class="member-info">Company:</label>
-                    <input type="text" class="member-info-input">
+                    <input name="company" type="text" class="member-info-input" id="memberCompanyRole">
                     <label class="member-info">Experience:</label>
-                    <select name="role-select" id="" class="member-info-input">
-                        <option value="">Récepionniste</option>
-                        <option value="">Technicien iT</option>
-                        <option value="">Agent de sécurité</option>
-                        <option value="">Manager</option>
-                        <option value="">Néttoyage</option>
+                    <select name="role" id="memberCompanyRole" class="member-info-input">
+                        <option >Récepionniste</option>
+                        <option >Technicien iT</option>
+                        <option >Agent de sécurité</option>
+                        <option >Manager</option>
+                        <option >Néttoyage</option>
                     </select>
+                    <label class="member-info">start:</label>
+                    <input name="date-start" type="date" class="member-info-input" id="dateStart">
+                    <span id="dateStartErr" class="validErr"></span>
+                    <label class="member-info">end:</label>
+                    <input name="date-end" type="date" class="member-info-input" id="dateEnd">
+                    <span id="dateStartErr" class="validErr"></span>
                     <button class="modal-ajout-buttons" onclick="return this.parentNode.remove()" >close</button>
 
                 `
-    expContainer.appendChild(template)    
-    
+
+    experiencesContainer.appendChild(template)
+    dateStart = document.getElementById("dateStart")
+    dateEnd = document.getElementById("dateEnd")
+    errorStartSpan = document.querySelectorAll("#dateStartErr")
+    expsContainer = document.querySelectorAll(".experiences-container")
+
 }
-function deleteExp(){ 
+function saveEmployesExp() {
+    let employésInfos = {
+        namee: "",
+        role: "",
+        url: ""
+    }
+    employésInfos.namee = namee.value
+    employésInfos.role = currentRole.value
+    employésInfos.url = url.value
+    employés.push(employésInfos)
+    let experiences = []
+
+
+    expsContainer.forEach((div) => {
+        let inputs = div.querySelectorAll('.member-info-input');
+        let experience = {}
+        inputs.forEach((input) => {
+            console.log(input);
+            experience[input.name] = input.value
+        })
+        experiences.push(experience)
+    })
+    employésInfos.experiences = experiences
+}
+function clearInputs() {
+    currentRole.value = ""
+    namee.value = ""
+    memberCompanyRole.value = ""
+    url.value = ""
+    expsContainer.forEach((div) => {
+        let inputs = div.querySelectorAll('#memberCompanyRole');
+        inputs.forEach((input) => {
+            input.value = ""
+        })
+    })
+
+
+
+}
+function appendImage() {
+    url.addEventListener("keyup", () => {
+        template = `<img src="${url.value}" alt="">`
+        memberImage.innerHTML = template
+    })
+}
+function appendImage2() {
+    template = `<img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="">`
+    memberImage.innerHTML = template
+}
+function dateCheck() {
+    let start = new Date(dateStart.value)
+    let end = new Date(dateEnd.value)
+    if (start > end) {
+        errorStartSpan.forEach((err) => {
+            err.textContent = "invalid date"
+        })
+        return true
+    }
+    else {
+        errorStartSpan.forEach((err) => {
+            err.textContent = ""
+        })
+        return false
+    }
 }
 function appInit() {
     addButton.addEventListener("click", () => {
         openModal(addModal)
     })
+    appendImage()
     expButton.addEventListener("click", () => {
         console.log("hello")
         experienceDiv()
@@ -91,7 +199,16 @@ function appInit() {
         e.preventDefault()
         nameCheck()
         urlCheck()
+        emailCheck()
+        numberCheck()
         regexCheck()
+        dateCheck()
+        if (!regexCheck() && !dateCheck()) {
+            saveEmployesExp()
+            appendImage2()
+            clearInputs()
+        }
+        console.log(employés)
     })
     closeAddModal.addEventListener("click", () => {
         closeModal(addModal)
